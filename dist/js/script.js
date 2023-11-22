f9a153aa400d76f97ab14e5208d47d43e7fa4d0c
Natalya <natalisaig@yandex.ru>
2023-11-22 21:45:54 +0600

    adds hamburger-active

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,38 +1,17 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    }
-    );
-});
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__nav'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.header__hamburger');
 
-const resultValueField = document.querySelector('.amount__result');
-const inputAmount = document.querySelector('.amount__slider');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('header__hamburger-active');
+        menu.classList.toggle('header__nav-active');
+    });
 
-
-inputAmount.addEventListener('input', () => {
-    resultValueField.textContent = inputAmount.value;
-});
-
-
-const resultValueTerm = document.querySelector('.term__result');
-const inputTerm = document.querySelector('.term__slider');
-
-
-inputTerm.addEventListener('input', () => {
-    resultValueTerm.textContent = inputTerm.value;
-});
-
-
-
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__nav-active');
+        })
+    })
+})
--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,9 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    }
-    );
-});
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__nav'),
+    hamburger = document.querySelector('.header__hamburger');
 
-const resultValueField = document.querySelector('.amount__result');
-const inputAmount = document.querySelector('.amount__slider');
-
-
-inputAmount.addEventListener('input', () => {
-    resultValueField.textContent = inputAmount.value;
-});
-
-
-const resultValueTerm = document.querySelector('.term__result');
-const inputTerm = document.querySelector('.term__slider');
-
-
-inputTerm.addEventListener('input', () => {
-    resultValueTerm.textContent = inputTerm.value;
-});
-
-
-
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('header__hamburger-active');
+        menu.classList.toggle('header__nav-active');
+    });
+})
