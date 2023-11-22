ab7b0621c44b51644f0787b2edb38a6afe7d9c95
Natalya <natalisaig@yandex.ru>
2023-11-23 03:15:34 +0600

    adds a modal window with video

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,8 +17,31 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollbar: {
             el: '.swiper-scrollbar',
         },
+        keyboard: true,
       });
 
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+      
+
+
+    
     
 })
 
